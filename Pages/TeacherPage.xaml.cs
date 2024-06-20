5afc04e4679470d9710b6ef94040de4148bb1fe2
white <white@PC>
2024-06-20 21:05:17 +1000

    teacher page

--- Pages/TeacherPage.xaml.cs
+++ Pages/TeacherPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using StudentProgress.Model;
+using StudentProgress.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,47 @@ namespace StudentProgress.Pages
         {
             InitializeComponent();
             DgTeacher.ItemsSource = StudentProgressEntities.GetContext().Teacher.ToList();
+        }
+
+        private void CreateBtnTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.SubForm.Navigate(new TeacherPageEdit(null));
+        }
+
+        private void EditBtnTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            var ObjectForEdit = DgTeacher.SelectedItem;
+            Manager.SubForm.Navigate(new TeacherPageEdit((Teacher)ObjectForEdit));
+        }
+
+        private void DeleteBtnTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            var StudentForDelete = DgTeacher.SelectedItems.Cast<Teacher>().ToList();
+            if (MessageBox.Show($"Вы действительно хотите удалить {StudentForDelete.Count()} элемент",
+                "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    StudentProgressEntities.GetContext().Teacher.RemoveRange(StudentForDelete);
+                    StudentProgressEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+
+                    DgTeacher.ItemsSource = StudentProgressEntities.GetContext().Teacher.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                StudentProgressEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DgTeacher.ItemsSource = StudentProgressEntities.GetContext().Teacher.ToList();
+            }
         }
     }
 }
---
+++ Pages/TeacherPageEdit.xaml.cs
@@ -0,0 +1,78 @@
+﻿using StudentProgress.Model;
+using StudentProgress.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentProgress.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TeacherPageEdit.xaml
+    /// </summary>
+    public partial class TeacherPageEdit : Page
+    {
+        private Teacher _currentTeacher = new Teacher();
+        public TeacherPageEdit(Teacher selectedTeacher)
+        {
+            InitializeComponent();
+            if (selectedTeacher != null)
+                _currentTeacher = selectedTeacher;
+
+            DataContext = _currentTeacher;
+            cmbSubject.ItemsSource = StudentProgressEntities.GetContext().Subject.ToList();
+        }
+
+        private void btnSaveTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (_currentTeacher.Surname == null)
+                errors = errors.AppendLine("Укажите фамилию преподавателя");
+            if (_currentTeacher.Name == null)
+                errors = errors.AppendLine("Укажите имя преподавателя");
+            if (_currentTeacher.Patronymic == null)
+                errors = errors.AppendLine("Укажите отчество преподавателя");
+            if (_currentTeacher.Post== null)
+                errors = errors.AppendLine("Укажите должность");
+            if (_currentTeacher.Subject == null)
+                errors = errors.AppendLine("Укажите номер телефона");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (_currentTeacher.ID_Teacher == 0)
+                try
+                {
+                    StudentProgressEntities.GetContext().Teacher.Add(_currentTeacher);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            try
+            {
+                StudentProgressEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
